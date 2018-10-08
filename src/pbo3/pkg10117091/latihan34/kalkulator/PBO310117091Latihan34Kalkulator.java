217f746214d4c295de189a7ef2e0c0f1486b61b8
Dida Handian <handiandida@gmail.com>
2018-10-08 22:17:24 +0700

    Kalkulator

---
+++ src/pbo3/pkg10117091/latihan34/kalkulator/PBO310117091Latihan34Kalkulator.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan34.kalkulator;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : 
+*/
+public class PBO310117091Latihan34Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kal.value1 = scn.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kal.value2 = scn.nextDouble();
+
+        //format dua digit di belakang koma
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        System.out.println("\nHasil Penjumlahan : "+kal.tambahBilangan());
+        System.out.println("Hasil Pengurangan : "+kal.kurangBiangan());
+        System.out.println("Hasil Perkalian : "+kal.kaliBilangan());
+        System.out.println("Hasil Pembagian : "+df.format(kal.bagiBilangan()));
+        System.out.println("Hasil Sisa : "+kal.sisaBilangan());
+
+    }
+    
+}
